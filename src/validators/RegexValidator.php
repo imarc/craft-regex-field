d163c8b2d75c36e4392bbd2792f94ce1e5d70702
Linnea <linneahartsuyker@imarc.com>
2023-07-18 10:57:28 -0400

    Removing logging statements

--- src/validators/RegexValidator.php
+++ src/validators/RegexValidator.php
@@ -28,17 +28,9 @@ class RegexValidator extends Validator
     protected function validateValue(mixed $value): ?array
     {
         $field = $this->field;
-        Craft::info('RegexFieldValidator::validateRegularExpression', "In method");
-
-        Craft::info('RegexFieldValidator::validateRegularExpression', "Value: " . json_encode($value));
 
         $customPattern = $field->pattern;
         $customPattern = '`'.$customPattern.'`';
-
-        Craft::info('RegexFieldValidator::validateRegularExpression', "Pattern: $customPattern");
-
-        Craft::info('RegexFieldValidator::validateRegularExpression', "Pregmatch: " . preg_match($customPattern, $value));
-
 
         if (!empty($customPattern)) {
             if (!preg_match($customPattern, $value)) {
